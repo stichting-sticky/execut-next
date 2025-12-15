53a06a7f836cd99c48d1fbf45e187e73c2b95b2b
Semele <hello@semele.fyi>
2025-12-15 22:09:48 +0100

    feat: add sitemap 🗺️

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from "astro/config";
 
+import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   build: { format: "file" },
+  integrations: [sitemap()],
   site: "https://execut.nl/",
   vite: { plugins: [tailwindcss()] },
 });
